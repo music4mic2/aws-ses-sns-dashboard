4ebc35c14ec471a0a1b9263f40c2cd641c51b71f
Andres Colonia <andreskal@gmail.com>
2016-03-03 12:04:02 -0300

    handlers to routes

---
+++ handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "net/http"
+)
+
+func Bounces(w http.ResponseWriter, r *http.Request) {
+}
+
+func Complaints(w http.ResponseWriter, r *http.Request) {
+}
+
+func Deliveries(w http.ResponseWriter, r *http.Request) {
+}
