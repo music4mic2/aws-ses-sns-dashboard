ce41aaa16b0d2728ae1e486ce0e509f9169acafa
Andres Colonia <andreskal@gmail.com>
2016-03-03 12:20:45 -0300

    routes to handlers

--- routes.go
+++ routes.go
@@ -5,27 +5,43 @@ import (
     "github.com/gorilla/mux"
 )
 
-
 type Route struct {
     Name        string
     Method      string
+    Pattern     string
     HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
+func NewRouter() *mux.Router {
+
+router := mux.NewRouter().StrictSlash(true)
+    for _, route := range routes {
+       router.
+         Methods(route.Method).
+         Path(route.Pattern).
+         Name(route.Name).
+         Handler(route.HandlerFunc)
+    }
+    return router
+}
+
 var routes = Routes{
   Route{
+      "Bounces",
       "/bounces",
       "POST",
       Bounces,
   },
     Route{
+      "Complaints",
       "/complaints",
       "POST",
       Complaints,
     },
     Route{
+      "Deliviries",
       "/Deliveries",
       "POST",
       Deliveries,
