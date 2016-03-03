c83cd5f4883f03a33c0ceb4c5f0025a48b5e3925
Andres Colonia <andreskal@gmail.com>
2016-03-03 16:08:25 -0300

    tab indentation

--- message.go
+++ message.go
@@ -1,15 +1,15 @@
 package main
 
 type Mail struct {
-  timestamp string
-  sendingAccountId string
-  source string
-  sourceArn string
-  messageId string
-  destination []string
+	timestamp string
+	sendingAccountId string
+	source string
+	sourceArn string
+	messageId string
+	destination []string
 }
 
 type Message struct {
-  notificationType string
-  mail Mail
+	notificationType string
+	mail Mail
 }
--- routes.go
+++ routes.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type Route struct {
-    Name        string
-    Pattern     string
-    Method      string
-    HandlerFunc http.HandlerFunc
+	Name        string
+	Pattern     string
+	Method      string
+	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 func NewRouter() *mux.Router {
 
-router := mux.NewRouter().StrictSlash(true)
-    for _, route := range routes {
-       router.
-         Methods(route.Method).
-         Path(route.Pattern).
-         Name(route.Name).
-         Handler(route.HandlerFunc)
-    }
-    return router
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+		Methods(route.Method).
+		Path(route.Pattern).
+		Name(route.Name).
+		Handler(route.HandlerFunc)
+	}
+	return router
 }
 
 var routes = Routes{
-  Route{
-      "Bounces",
-      "/bounces",
-      "POST",
-      Bounces,
-  },
-    Route{
-      "Complaints",
-      "/complaints",
-      "POST",
-      Complaints,
-    },
-    Route{
-      "Deliviries",
-      "/Deliveries",
-      "POST",
-      Deliveries,
-    },
+	Route{
+		"Bounces",
+		"/bounces",
+		"POST",
+		Bounces,
+	},
+	Route{
+		"Complaints",
+		"/complaints",
+		"POST",
+		Complaints,
+	},
+	Route{
+		"Deliviries",
+		"/Deliveries",
+		"POST",
+		Deliveries,
+	},
 }
