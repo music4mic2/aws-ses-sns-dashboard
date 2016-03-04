a19c355dd25545847dae11bd78eee0fa388afab5
Andres Colonia <andreskal@gmail.com>
2016-03-04 14:27:32 -0300

    handler request bounce and save into postgres

--- handlers.go
+++ handlers.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
-func Bounces(w http.ResponseWriter, r *http.Request) {
+func Bounces(res http.ResponseWriter, req *http.Request) {
+
+	decoder := json.NewDecoder(req.Body)
+	var bounceType BounceType
+	err := decoder.Decode(&bounceType)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := connectDB()
+
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("failed connect DB")
+	}
+
+	db.DB()
+	log.Println(db.DB().Ping())
+	db.CreateTable(&bounceType)
+	db.Create(&bounceType)
 }
 
 func Complaints(w http.ResponseWriter, r *http.Request) {
 }
 
-func Deliveries(w http.ResponseWriter, r *http.Request) {
+func Deliveries(res http.ResponseWriter, req *http.Request) {
+}
+
+func connectDB() (gorm.DB, error) {
+	db, error := gorm.Open("postgres", "user=postgres dbname=notifications password=password host=localhost port=5433 sslmode=disable")
+	log.Println(db.DB().Ping())
+	return db, error
 }
