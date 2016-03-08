fddf6d059ff775fb31acf37e281196eac37d787e
Andres Colonia <andreskal@gmail.com>
2016-03-08 17:19:19 -0300

    separate variables to postgres

--- db.go
+++ db.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 
+	"encoding/json"
+	"os"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,8 +14,26 @@ var bounce Bounce
 var delivery Delivery
 var notification Notification
 
+type Configuration struct {
+	Adapter  string
+	User     string
+	Database string
+	Password string
+	Host     string
+	Port     string
+}
+
 func connectDB() (gorm.DB, error) {
-	db, error := gorm.Open("postgres", "user=postgres dbname=notifications password=password host=localhost port=5433 sslmode=disable")
+
+	var configuration Configuration
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+
+	if err := decoder.Decode(&configuration); err != nil {
+		log.Println(err)
+	}
+
+	db, error := gorm.Open(configuration.Adapter, "user="+configuration.User+" dbname="+configuration.Database+" password="+configuration.Password+" host="+configuration.Host+" port="+configuration.Port+" sslmode=disable")
 	return db, error
 }
 
