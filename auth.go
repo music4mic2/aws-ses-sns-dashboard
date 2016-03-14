eb00e732ec862e2069f2892787c0de16a1893b6a
Andres Colonia <andreskal@gmail.com>
2016-03-14 11:54:01 -0300

    basic authenticate

---
+++ auth.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+func check(w http.ResponseWriter, r *http.Request) bool {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+
+	return pair[0] == "admin" && pair[1] == "admin"
+}
