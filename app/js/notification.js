d267ba7486861022cd9f62e79e49bf05f2bd2901
Andres Colonia <andreskal@gmail.com>
2016-03-16 11:50:37 -0300

    filter to dashboard

--- app/js/notification.js
+++ app/js/notification.js
@@ -1,22 +1,80 @@
-var NotificationComponent = React.createClass({
-  getInitialState: function() {
-    return { list: {} };
-  },
-  componentDidMount: function() {
-  var _this = this;
-
-  //TODO
-  $.get( "http://localhost:8000/notifications", { page: 1, limit: 2 }, function(data){})
-    .done(function(data) {
-      _this.setState( { list: data })
-    });
+var PaginationComponent = React.createClass({
+  getDefaultProps: function() {
+    page: 1
   },
   render: function() {
-    if ( this.state.list.length > 0 ) {
+    return (
+      <ul className="pagination">
+        <li><a href= "javascript:void(0);" onClick={this.clickHandler.bind(this,this.props.page - 1)}>&laquo;</a></li>
+        <li><a href= "javascript:void(0);" onClick={this.clickHandler.bind(this,this.props.page + 1)}>&raquo;</a></li>
+      </ul>
+    )
+  },
+  clickHandler: function(page, e) {
+    $(".pagination").removeClass("active");
+    $("li.pagination:eq("+ (page - 1) +")").addClass("active");
+    var email = this.props.email;
+    this.props.fetchList(page, email);
+  }
+});
+
+var FilterComponent = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <div className="form-group">
+          <div className="col-sm-4">
+            <div className="input-group"><input type="email" placeholder="Email" className="input-sm form-control" id="email" required />
+              <span className="input-group-btn">
+                <button type="button" className="btn btn-sm btn-primary" onClick={this.clickHandler}>Buscar</button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  },
+   clickHandler: function(){
+     var email = $("#email").val();
+     var page = this.props.page;
+     this.props.fetchList(page, email);
+   }
+});
+
+var NotificationComponent = React.createClass({
+  getInitialState: function() {
+    return { 
+      list: {}, 
+      page: 1, 
+      email: null
+    };
+  },
+  fetchList: function(page, email){
+    var _this = this;
+    var page = page || 1;
+    var email = email || "";
+
+    //TODO
+    $.get( "http://localhost:8000/notifications", { page: page, email: email }, function(data){})
+    .done(function(data) {
+      _this.setState({ 
+        list: data, 
+        page: page, 
+        email: email 
+      });
+    });
+  },
+  componentDidMount: function() {
+    this.fetchList();
+  },
+  render: function() {
+    var list_length = this.state.list.length;
+    if ( list_length > 0 ) {
       var notifications = this.state.list.map(function(value, index){
         var date = new Date(value.CreatedAt);
         return (
           <tr key={index}>
+            <td>{value.MailID}</td>
             <td>{value.notificationType == "Delivery" ? <span className="label label-primary">{value.notificationType}</span> : <span className="label label-danger">{value.notificationType}</span>}<br /><small className="text-muted">{ date.toUTCString() }</small></td>
             <td>{ value.mail.destination.join(", ") }</td>
             <td>{ value.mail.source }</td>
@@ -25,25 +83,31 @@ var NotificationComponent = React.createClass({
         )
       });
     return (
-        <table className="table table-striped">
-          <thead>
-          <tr>
-            <th>Notificación</th>
-            <th>Email</th>
-            <th>Fuente</th>
-            <th>Detalle</th>
-          </tr>
-          </thead>
-          <tbody>
-            { notifications }
-          </tbody>
-        </table>
-        );
+      <div>
+        <FilterComponent fetchList={this.fetchList} page={this.state.page}/>
+        <div className="table-responsive">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>Notificación</th>
+                <th>Email</th>
+                <th>Fuente</th>
+                <th>Detalle</th>
+              </tr>
+            </thead>
+            <tbody>
+              { notifications }
+            </tbody>
+          </table>
+          <PaginationComponent fetchList={this.fetchList} page={this.state.page} email={this.state.email}/>
+        </div>
+      </div>
+    );
   }
   else {
-    return (<span></span>)
+    return (<div className="alert alert-info">No existen mas notificaciones.</div>)
     }
   },
 });
-
 ReactDOM.render(<NotificationComponent/>, document.getElementById('content'));
