313dc85c9beb5739973ce63a6978fda06ffdce5f
Andres Colonia <andreskal@gmail.com>
2016-03-17 18:47:07 -0300

    where filter by email

--- handlers.go
+++ handlers.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -12,6 +12,13 @@ import (
 func Notifications(res http.ResponseWriter, req *http.Request) {
 
 	var notification Notification
+
+	if !check(res, req) {
+		res.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
+		res.WriteHeader(401)
+		res.Write([]byte("401 Unauthorized\n"))
+		return
+	}
 
 	decoder := json.NewDecoder(req.Body)
 	if err := decoder.Decode(&notification); err != nil {
@@ -25,6 +32,19 @@ func Notifications(res http.ResponseWriter, req *http.Request) {
 }
 
 func NotificationIndex(res http.ResponseWriter, req *http.Request) {
+
+	const limit = 100
+
+	user, pass, _ := req.BasicAuth()
+	log.Println(user)
+	log.Println(pass)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	res.Header().Set("Access-Control-Allow-Credentials", "true")
+	res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, Accept")
+
 	if !check(res, req) {
 		res.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
 		res.WriteHeader(401)
@@ -32,35 +52,28 @@ func NotificationIndex(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	page, _ := strconv.Atoi(req.URL.Query().Get("page"))
+	email := req.URL.Query().Get("email")
+
+	if page == 0 {
+		page++
+	}
+
 	var notifications []Notification
 
 	db := connectDB()
 	db.DB()
 	db.LogMode(true)
-	db.Preload("Mail").Find(&notifications)
+
+	db.Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Preload("Mail").Preload("Bounce").Find(&notifications)
+	if email != "" {
+		db.Where("mails.destination LIKE ?", "%"+email+"%").Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Joins("JOIN mails on mails.id = notifications.mail_id").Preload("Mail").Preload("Bounce").Find(&notifications)
+	}
 
 	json, err := json.Marshal(notifications)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	res.Header().Set("Content-Type", "application/json")
-	res.Header().Set("Access-Control-Allow-Origin", "*");
 	res.Write(json)
-}
-
-func Stylesheets() {
-	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
-}
-
-//html views
-func Dashboard(res http.ResponseWriter, req *http.Request) {
-	var notification Notification
-	db := connectDB()
-	db.DB()
-
-	tmpl, _ := template.ParseFiles("views/dashboard.html")
-	tmpl.Execute(res, notification)
-
-	res.Header().Set("Content-Type", "text/html")
 }
