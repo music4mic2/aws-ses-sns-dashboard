3f7d04656b92ec4ac0b7db31d80476e8e0d8f224
Andres Colonia <andreskal@gmail.com>
2016-03-18 14:33:10 -0300

    add subscribe and unsubscribe option

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,22 +14,24 @@ func Notifications(res http.ResponseWriter, req *http.Request) {
 
 	var notification Notification
 
-	if !check(res, req) {
-		res.Header().Set("WWW-Authenticate", `Basic realm="beetrack.com"`)
-		res.WriteHeader(401)
-		res.Write([]byte("401 Unauthorized\n"))
-		return
-	}
+	if checkAuth(res, req) {
 
-	decoder := json.NewDecoder(req.Body)
-	if err := decoder.Decode(&notification); err != nil {
-		log.Println(err)
-	}
+		if SubscriptionConfirmation(res, req) {
+			res.WriteHeader(200)
+			res.Write([]byte("200 OK\n"))
+			return
+		} else {
+			decoder := json.NewDecoder(req.Body)
+			if err := decoder.Decode(&notification); err != nil {
+				log.Println(err)
+			}
 
-	db := connectDB()
-	db.DB()
-	db.LogMode(true)
-	db.Create(&notification)
+			db := connectDB()
+			db.DB()
+			db.LogMode(true)
+			db.Create(&notification)
+		}
+	}
 }
 
 func NotificationIndex(res http.ResponseWriter, req *http.Request) {
@@ -45,35 +48,69 @@ func NotificationIndex(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Access-Control-Allow-Credentials", "true")
 	res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, Accept")
 
+	if checkAuth(res, req) {
+
+		page, _ := strconv.Atoi(req.URL.Query().Get("page"))
+		email := req.URL.Query().Get("email")
+
+		if page == 0 {
+			page++
+		}
+
+		var notifications []Notification
+
+		db := connectDB()
+		db.DB()
+		db.LogMode(true)
+
+		db.Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Preload("Mail").Preload("Bounce").Find(&notifications)
+		if email != "" {
+			db.Where("mails.destination LIKE ?", "%"+email+"%").Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Joins("JOIN mails on mails.id = notifications.mail_id").Preload("Mail").Preload("Bounce").Find(&notifications)
+		}
+
+		json, err := json.Marshal(notifications)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		res.Write(json)
+	}
+}
+
+func SubscriptionConfirmation(res http.ResponseWriter, req *http.Request) bool {
+
+	if req.Header.Get("x-amz-sns-message-type") != "" {
+		body, err := ioutil.ReadAll(req.Body)
+
+		if err != nil {
+			log.Println(err)
+		}
+		mapper := make(map[string]interface{})
+		e := json.Unmarshal(body, &mapper)
+
+		log.Println(e)
+
+		switch req.Header.Get("x-amz-sns-message-type") {
+		case "SubscriptionConfirmation":
+			log.Println(mapper["SubscribeURL"])
+			return true
+		case "Notification":
+			log.Println(mapper["UnsubscribeURL"])
+
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+func checkAuth(res http.ResponseWriter, req *http.Request) bool {
 	if !check(res, req) {
 		res.Header().Set("WWW-Authenticate", `Basic realm="beetrack.com"`)
 		res.WriteHeader(401)
 		res.Write([]byte("401 Unauthorized\n"))
-		return
+		return false
+	} else {
+		return true
 	}
-
-	page, _ := strconv.Atoi(req.URL.Query().Get("page"))
-	email := req.URL.Query().Get("email")
-
-	if page == 0 {
-		page++
-	}
-
-	var notifications []Notification
-
-	db := connectDB()
-	db.DB()
-	db.LogMode(true)
-
-	db.Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Preload("Mail").Preload("Bounce").Find(&notifications)
-	if email != "" {
-		db.Where("mails.destination LIKE ?", "%"+email+"%").Offset((page - 1) * limit).Limit(limit).Order("created_at asc").Joins("JOIN mails on mails.id = notifications.mail_id").Preload("Mail").Preload("Bounce").Find(&notifications)
-	}
-
-	json, err := json.Marshal(notifications)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	res.Write(json)
 }
