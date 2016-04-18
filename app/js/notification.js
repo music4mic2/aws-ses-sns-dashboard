abed1d0b84d17ece4bbbf01cbe53d16d9c8b5848
Andres Colonia <andreskal@gmail.com>
2016-04-18 16:19:56 -0300

    add content email

--- app/js/notification.js
+++ app/js/notification.js
@@ -102,6 +102,7 @@ var NotificationComponent = React.createClass({
           <tr key={index}>
             <td>{value.notificationType == "Delivery" ? <span className="label label-primary">{value.notificationType}</span> : <span className="label label-danger">{value.notificationType}</span>}<br /><small className="text-muted">{ date.toLocaleString() }</small></td>
             <td>{ value.mail.destination.join(", ") }</td>
+            <td><a href={'https://s3.amazonaws.com/beetrack-shared/emails/' +  value.mail.message_id} sorce target="_blank"><i class="fa fa-search" aria-hidden="true"></i></a></td>
             <td>{ value.mail.source }</td>
           </tr>
         )
@@ -116,6 +117,7 @@ var NotificationComponent = React.createClass({
                 <th>Notificación</th>
                 <th>Email</th>
                 <th>Fuente</th>
+                <th>Contenido</th>
               </tr>
             </thead>
             <tbody>
